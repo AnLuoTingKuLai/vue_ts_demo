7a610dac727e8de30a3a302ac10ff75867d56ff2
chendd <869316456@qq.com>
2019-02-20 16:07:16 +0800

    添加颜色

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="home">
+    <el-button class="btn-switch-bg" @click="changeBg"
+      >使用 --切换背景</el-button
+    >
+
     <el-form ref="form" :model="form" label-width="80px">
       <el-form-item label="标题1">
         <el-input v-model="form.title"></el-input>
@@ -31,16 +35,28 @@ interface formData {
   }
 })
 export default class Home extends Vue {
+  bgSwitch: Boolean = false;
   form: formData = {
     title: "标题",
     content: "内容",
     msg: ""
   };
   textContent: String | Number = 123;
+  changeBg() {
+    this.bgSwitch = !this.bgSwitch;
+    let doc: any = document.documentElement;
+    doc.style.setProperty("--bg", this.bgSwitch ? "#ececec" : "#fff");
+  }
 }
 </script>
 <style lang="scss">
-.home {
-  width: 400px;
+body {
+  background: var(--bg);
+  .home {
+    width: 400px;
+  }
+  .btn-switch-bg {
+    margin-bottom: 15px;
+  }
 }
 </style>
--- src/views/home/components/content.vue
+++ src/views/home/components/content.vue
@@ -103,3 +103,4 @@ export default class HomeContent extends Vue implements HomeContent2 {
   }
 }
 </script>
+<style lang="scss"></style>
